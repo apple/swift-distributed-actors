7f26fc211b4951ea66a539cc674b17891ba38427
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2018-11-30 13:44:37 +0900

    flags for enabling traces

--- Sources/DistributedActorsActor/utils+internal.swift
+++ Sources/DistributedActorsActor/utils+internal.swift
@@ -30,15 +30,21 @@ internal extension ActorRef {
 
 /// INTERNAL API: Used for easier debugging; most of those messages are meant to be eventually removed
 func traceLog_DeathWatch(_ message: @autoclosure () -> String, file: StaticString = #file, line: UInt = #line) {
+    #if SACT_TRACE_DEATHWATCH
     pprint(message(), file: file, line: line)
+    #endif
 }
 
 /// INTERNAL API: Used for easier debugging; most of those messages are meant to be eventually removed
 func traceLog_Mailbox(_ message: @autoclosure () -> String, file: StaticString = #file, line: UInt = #line) {
+    #if SACT_TRACE_MAILBOX
     pprint(message(), file: file, line: line)
+    #endif
 }
 
 /// INTERNAL API: Used for easier debugging; most of those messages are meant to be eventually removed
 func traceLog_Cell(_ message: @autoclosure () -> String, file: StaticString = #file, line: UInt = #line) {
+    #if SACT_TRACE_CELL
     pprint(message(), file: file, line: line)
+    #endif
 }
