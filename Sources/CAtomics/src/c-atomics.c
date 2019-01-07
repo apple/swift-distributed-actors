d6ae62d745afa0d65d86cfc134412156871f5f21
Dario Rexin <drexin@apple.com>
2019-01-07 11:33:54 -0800

    Add memory_order and weak exchange to atomics

--- Sources/CAtomics/src/c-atomics.c
+++ Sources/CAtomics/src/c-atomics.c
@@ -42,9 +42,9 @@ struct __catmc_atomic_flag *__catmc_atomic_flag_create(bool value) {
     struct __catmc_atomic_flag *flag = malloc(sizeof(*flag));
     flag->_flag = (__typeof__(flag->_flag))ATOMIC_FLAG_INIT;
     if (value) {
-        (void)atomic_flag_test_and_set_explicit(&flag->_flag, memory_order_relaxed);
+        (void)atomic_flag_test_and_set_explicit(&flag->_flag, memory_order_seq_cst);
     } else {
-        atomic_flag_clear_explicit(&flag->_flag, memory_order_relaxed);
+        atomic_flag_clear_explicit(&flag->_flag, memory_order_seq_cst);
     }
     return flag;
 }
@@ -68,41 +68,46 @@ void __catmc_atomic_flag_destroy(struct __catmc_atomic_flag *flag) {
 */     free(wrapper); /*
 */ } /*
 */ /*
-*/ bool __catmc_atomic_##type##_compare_and_exchange(struct __catmc_atomic_##type *wrapper, type expected, type desired) { /*
+*/ bool __catmc_atomic_##type##_compare_and_exchange_strong(struct __catmc_atomic_##type *wrapper, type expected, type desired, memory_order succ, memory_order fail) { /*
 */     type expected_copy = expected; /*
-*/     return atomic_compare_exchange_strong(&wrapper->value, &expected_copy, desired); /*
+*/     return atomic_compare_exchange_strong_explicit(&wrapper->value, &expected_copy, desired, succ, fail); /*
 */ } /*
 */ /*
-*/ type __catmc_atomic_##type##_add(struct __catmc_atomic_##type *wrapper, type value) { /*
-*/     return atomic_fetch_add_explicit(&wrapper->value, value, memory_order_relaxed); /*
+*/ bool __catmc_atomic_##type##_compare_and_exchange_weak(struct __catmc_atomic_##type *wrapper, type expected, type desired, memory_order succ, memory_order fail) { /*
+*/     type expected_copy = expected; /*
+*/     return atomic_compare_exchange_weak_explicit(&wrapper->value, &expected_copy, desired, succ, fail); /*
 */ } /*
 */ /*
-*/ type __catmc_atomic_##type##_sub(struct __catmc_atomic_##type *wrapper, type value) { /*
-*/     return atomic_fetch_sub_explicit(&wrapper->value, value, memory_order_relaxed); /*
+*/ type __catmc_atomic_##type##_add(struct __catmc_atomic_##type *wrapper, type value, memory_order order) { /*
+*/     return atomic_fetch_add_explicit(&wrapper->value, value, order); /*
 */ } /*
 */ /*
-*/ type __catmc_atomic_##type##_exchange(struct __catmc_atomic_##type *wrapper, type value) { /*
-*/     return atomic_exchange_explicit(&wrapper->value, value, memory_order_relaxed); /*
+*/ type __catmc_atomic_##type##_sub(struct __catmc_atomic_##type *wrapper, type value, memory_order order) { /*
+*/     return atomic_fetch_sub_explicit(&wrapper->value, value, order); /*
 */ } /*
 */ /*
-*/ type __catmc_atomic_##type##_load(struct __catmc_atomic_##type *wrapper) { /*
-*/     return atomic_load_explicit(&wrapper->value, memory_order_relaxed); /*
+*/ type __catmc_atomic_##type##_exchange(struct __catmc_atomic_##type *wrapper, type value, memory_order order) { /*
+*/     return atomic_exchange_explicit(&wrapper->value, value, order); /*
 */ } /*
 */ /*
-*/ void __catmc_atomic_##type##_store(struct __catmc_atomic_##type *wrapper, type value) { /*
-*/     atomic_store_explicit(&wrapper->value, value, memory_order_relaxed); /*
+*/ type __catmc_atomic_##type##_load(struct __catmc_atomic_##type *wrapper, memory_order order) { /*
+*/     return atomic_load_explicit(&wrapper->value, order); /*
 */ } /*
 */ /*
-*/ type __catmc_atomic_##type##_and(struct __catmc_atomic_##type *wrapper, type value) { /*
-*/     return atomic_fetch_and_explicit(&wrapper->value, value, memory_order_relaxed); /*
+*/ void __catmc_atomic_##type##_store(struct __catmc_atomic_##type *wrapper, type value, memory_order order) { /*
+*/     atomic_store_explicit(&wrapper->value, value, order); /*
 */ } /*
 */ /*
-*/ type __catmc_atomic_##type##_or(struct __catmc_atomic_##type *wrapper, type value) { /*
-*/     return atomic_fetch_or_explicit(&wrapper->value, value, memory_order_relaxed); /*
+*/ type __catmc_atomic_##type##_and(struct __catmc_atomic_##type *wrapper, type value, memory_order order) { /*
+*/     return atomic_fetch_and_explicit(&wrapper->value, value, order); /*
 */ } /*
 */ /*
-*/ type __catmc_atomic_##type##_xor(struct __catmc_atomic_##type *wrapper, type value) { /*
-*/     return atomic_fetch_xor_explicit(&wrapper->value, value, memory_order_relaxed); /*
+*/ type __catmc_atomic_##type##_or(struct __catmc_atomic_##type *wrapper, type value, memory_order order) { /*
+*/     return atomic_fetch_or_explicit(&wrapper->value, value, order); /*
+*/ } /*
+*/ /*
+*/ type __catmc_atomic_##type##_xor(struct __catmc_atomic_##type *wrapper, type value, memory_order order) { /*
+*/     return atomic_fetch_xor_explicit(&wrapper->value, value, order); /*
 */ }
 
 
