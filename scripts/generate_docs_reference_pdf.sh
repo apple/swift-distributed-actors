fb5b071a0367687bd051d4c051801d82b01fd459
Konrad `ktoso` Malawski <ktoso@apple.com>
2019-02-01 18:54:47 +0900

    Docs also available as PDF now

---
+++ scripts/generate_docs_reference_pdf.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the SwiftNIO open source project
+##
+## Copyright (c) 2017-2018 Apple Inc. and the SwiftNIO project authors
+## Licensed under Apache License v2.0
+##
+## See LICENSE.txt for license information
+## See CONTRIBUTORS.md for the list of SwiftNIO project authors
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+##===----------------------------------------------------------------------===##
+
+
+set -e
+
+declare -r my_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+declare -r root_path="$my_path/.."
+declare -r version=$(git describe --abbrev=0 --tags 2> /dev/null || echo "0.0.0")
+
+# run asciidoctor
+if ! command -v asciidoctor-pdf > /dev/null; then
+  gem install asciidoctor --no-ri --no-rdoc
+  gem install asciidoctor-pdf --pre
+fi
+
+declare -r target_dir="$root_path/reference/$version"
+
+asciidoctor-pdf \
+  -D $target_dir \
+  $root_path/Sources/Docs/index.adoc
+
+mv $target_dir/index.pdf $target_dir/swift-distributed-actors-reference.pdf
+
+echo "PDF docs generated: $target_dir/swift-distributed-actors-reference.pdf"
