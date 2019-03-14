3c4209af9bad365085ab0e4a827407168eae249d
Konrad Malawski <konrad_malawski@apple.com>
2019-03-14 13:39:35 +0900

    Update validate_docs.sh

--- scripts/validate_docs.sh
+++ scripts/validate_docs.sh
@@ -6,8 +6,13 @@ swift package generate-xcodeproj
 
 bash ./scripts/generate_docs_api.sh
 
-ref_out=$(./scripts/generate_docs_reference.sh)
+ref_out=$(./scripts/generate_docs_reference.sh 2>&1)
 if [[ "$ref_out" == *"NOT FOUND"* ]];
 then
+  echo "Docs validation FAILED!"
+  echo "======== NOT FOUND ISSUES ========="
+  echo "$ref_out" | grep "NOT FOUND"
+  echo "===== END OF NOT FOUND ISSUES ====="
+  echo "$ref_out"
   exit 1
 fi
