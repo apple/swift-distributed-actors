46822e13599255d5cc3b735ef6eb3f6982ede1fd
Konrad `ktoso` Malawski <ktoso@apple.com>
2019-03-27 17:18:43 +0900

    +string leftPadTo string interpolation, for nicer printouts

---
+++ Sources/DistributedActorsActor/CustomStringInterpolations.swift
@@ -0,0 +1,21 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Distributed Actors open source project
+//
+// Copyright (c) 2018-2019 Apple Inc. and the Swift Distributed Actors project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.md for the list of Swift Distributed Actors project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+extension String.StringInterpolation {
+    mutating func appendInterpolation(_ value: CustomStringConvertible, leftPadTo totalLength: Int) {
+        let s = "\(value)"
+        let pad = String(repeating: " ", count: max(totalLength - s.count, 0))
+        self.appendLiteral("\(pad)\(s)")
+    }
+}
