39b414016fccce10e5eb7acffad22558e554bf2b
Konrad `ktoso` Malawski <ktoso@apple.com>
2019-04-03 22:58:47 +0900

    fix

--- Sources/DistributedActorsSampleDiningPhilosophers/main.swift
+++ Sources/DistributedActorsSampleDiningPhilosophers/main.swift
@@ -36,14 +36,6 @@ let fork5: Fork.Ref = try system.spawn(Fork.behavior, name: "fork-5")
 
 // 5 philosophers, sitting in a circle, with the forks between them:
 let p1: Philosopher.Ref = try system.spawn(Philosopher(left: fork5, right: fork1).start, name: "Konrad")
-
-p1.tell(.think)
-p1.tell(.think)
-p1.tell(.think)
-p1.tell(.think)
-p1.tell(.think)
-p1.tell(.think)
-
 let p2: Philosopher.Ref = try system.spawn(Philosopher(left: fork1, right: fork2).start, name: "Dario")
 let p3: Philosopher.Ref = try system.spawn(Philosopher(left: fork2, right: fork3).start, name: "Johannes")
 let p4: Philosopher.Ref = try system.spawn(Philosopher(left: fork3, right: fork4).start, name: "Cory")
