9acfb4952a7208ec777aa8fde4419f73d48cff39
Konrad `ktoso` Malawski <ktoso@apple.com>
2019-04-10 00:53:05 +0900

    =docs initial note about how to join nodes

---
+++ Tests/DistributedActorsDocumentationTests/ClusterDocExamples.swift
@@ -0,0 +1,39 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Distributed Actors open source project
+//
+// Copyright (c) 2018-2019 Apple Inc. and the Swift Distributed Actors project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.md for the list of Swift Distributed Actors project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+// tag::imports[]
+
+import Swift Distributed ActorsActor
+
+// end::imports[]
+
+import XCTest
+@testable import SwiftDistributedActorsActorTestKit
+
+class ClusterDocExamples: XCTestCase {
+
+    func example_receive_behavior() throws {
+        // tag::joining[]
+        let system = ActorSystem("ClusterJoining") { settings in 
+            settings.cluster.enabled = true // <1>
+            // system will bind by default on `localhost:7337`
+        }
+
+        let otherNodeAddress = NodeAddress(systemName: "ClusterJoining", host: "localhost", port: 8228)
+        system.join(address: otherNodeAddress) // TODO not final API // <2>
+        // TODO provide a proper API for this; perhaps system.tell(.cluster(.join)) or similar ?
+
+        // end::joining[]
+    }
+}
