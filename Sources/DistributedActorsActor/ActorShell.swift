abf02656d3045f47da1ba978810738a89e38fa74
Konrad Malawski <konrad_malawski@apple.com>
2019-08-08 07:25:10 +0900

    =mailbox do not leak mailboxes, tombstone processing must release mai…
    * =mailbox do not leak mailboxes, tombstone processing must release mailbox ##893
    
    * Use self._run instead of self.run in Mailbox
    
    * Fix typo
    
    * Fix ActorLeakingTests after stopped -> stop rename
    
    * Update ActorLeakingTests.swift

--- Sources/DistributedActorsActor/ActorShell.swift
+++ Sources/DistributedActorsActor/ActorShell.swift
@@ -491,7 +491,7 @@ internal final class ActorShell<Message>: ActorContext<Message>, AbstractActor {
             self.log.error("Exception in postStop. Supervision will NOT be applied. Error \(error)")
         }
 
-        // TODO validate all the nulling out; can we null out the cell itself?
+        // TODO validate all the niling out; can we null out the cell itself?
         self._deathWatch = nil
         self.messageAdapters = [:]
 
