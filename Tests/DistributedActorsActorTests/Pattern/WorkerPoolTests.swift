31855f2a3812277294f5001931ac0d3f2b0d36e0
Konrad Malawski <konrad_malawski@apple.com>
2019-08-09 16:01:42 +0900

    =workerpool #864 Harden tests for dynamic selector, add static selector
    * =workerpool #864 Harden tests for dynamic selector, add static selector
    
    * Introduce static selector for WorkerPool, add tests for crashing
    
    * Improve timing of , within: worker pool tests
    
    * address all review

--- Tests/DistributedActorsActorTests/Pattern/WorkerPoolTests.swift
+++ Tests/DistributedActorsActorTests/Pattern/WorkerPoolTests.swift
@@ -14,7 +14,7 @@
 
 import Foundation
 import XCTest
-import Swift Distributed ActorsActor
+@testable import Swift Distributed ActorsActor
 import SwiftDistributedActorsActorTestKit
 
 // TODO "ActorGroup" perhaps could be better name?
@@ -76,7 +76,7 @@ final class WorkerPoolTests: XCTestCase {
         try pC.expectNoMessage(for: .milliseconds(50))
     }
 
-    func test_workerPool_removeDeadActors() throws {
+    func test_workerPool_dynamic_removeDeadActors() throws {
         let workerKey = Receptionist.RegistrationKey(String.self, id: "request-workers")
 
         let pA: ActorTestProbe<String> = testKit.spawnTestProbe(name: "pA")
@@ -106,13 +106,20 @@ final class WorkerPoolTests: XCTestCase {
 
         let workers = try WorkerPool.spawn(system, select: .dynamic(workerKey), name: "workersMayDie")
 
-        workers.tell("a")
-        workers.tell("b")
-        workers.tell("c")
-        try pA.expectMessage("work:a at worker-a")
-        try pB.expectMessage("work:b at worker-b")
-        try pC.expectMessage("work:c at worker-c")
-
+        // since the workers joining the pool is still technically always a bit racy with the dynamic selection,
+        // we try a few times to send a message and see it delivered at each worker. This would not be the case with a static selector.
+        try testKit.eventually(within: .seconds(1)) {
+            workers.tell("a")
+            try pA.expectMessage("work:a at worker-a", within: .milliseconds(200))
+        }
+        try testKit.eventually(within: .seconds(1)) {
+            workers.tell("b")
+            try pB.expectMessage("work:b at worker-b", within: .milliseconds(200))
+        }
+        try testKit.eventually(within: .seconds(1)) {
+            workers.tell("c")
+            try pC.expectMessage("work:c at worker-c", within: .milliseconds(200))
+        }
         workerA.tell("stop")
         try pA.expectTerminated(workerA)
         // inherently this is racy, if a worker dies it may have taken a message with it
@@ -124,8 +131,8 @@ final class WorkerPoolTests: XCTestCase {
         for i in 0...2 {
             try testKit.eventually(within: .seconds(1)) {
                 workers.tell("after-A-dead-\(i)")
-                let maybeBGotIt = try pB.maybeExpectMessage()
-                let maybeCGotIt = try pC.maybeExpectMessage()
+                let maybeBGotIt = try pB.maybeExpectMessage(within: .milliseconds(200))
+                let maybeCGotIt = try pC.maybeExpectMessage(within: .milliseconds(200))
 
                 // one of the workers should have handled it
                 (maybeBGotIt != nil || maybeCGotIt != nil).shouldBeTrue()
@@ -136,5 +143,76 @@ final class WorkerPoolTests: XCTestCase {
             }
         }
         try pA.expectNoMessage(for: .milliseconds(50))
+    }
+
+    func test_workerPool_static_removeDeadActors_terminateItselfWhenNoWorkers() throws {
+        let pA: ActorTestProbe<String> = testKit.spawnTestProbe(name: "pA")
+        let pB: ActorTestProbe<String> = testKit.spawnTestProbe(name: "pB")
+        let pC: ActorTestProbe<String> = testKit.spawnTestProbe(name: "pC")
+        let pW: ActorTestProbe<String> = testKit.spawnTestProbe(name: "pW")
+
+        func worker(p: ActorTestProbe<String>) -> Behavior<String> {
+            return .receive { context, work in
+                if work == "stop" {
+                    return .stop
+                }
+                p.tell("work:\(work) at \(context.path.name)")
+                return .same
+            }
+        }
+
+        let workerA = try system.spawn(worker(p: pA), name: "worker-a")
+        pA.watch(workerA)
+        let workerB = try system.spawn(worker(p: pB), name: "worker-b")
+        pB.watch(workerB)
+        let workerC = try system.spawn(worker(p: pC), name: "worker-c")
+        pC.watch(workerC)
+
+        let workers = try WorkerPool.spawn(system, select: .static([workerA, workerB, workerC]), name: "staticWorkersMayDie")
+        pW.watch(workers._ref)
+
+        // since using a static pool, we know the messages will arrive; no need to wait for the receptionist dance:
+        workers.tell("a")
+        try pA.expectMessage("work:a at worker-a")
+        workers.tell("b")
+        try pB.expectMessage("work:b at worker-b")
+        workers.tell("c")
+        try pC.expectMessage("work:c at worker-c")
+
+        workerA.tell("stop")
+        try pA.expectTerminated(workerA)
+
+        for i in 0...2 {
+            try testKit.eventually(within: .seconds(1)) {
+                workers.tell("after-A-dead-\(i)")
+                let maybeBGotIt = try pB.maybeExpectMessage(within: .milliseconds(200))
+                let maybeCGotIt = try pC.maybeExpectMessage(within: .milliseconds(200))
+
+                // one of the workers should have handled it
+                (maybeBGotIt != nil || maybeCGotIt != nil).shouldBeTrue()
+                // but NOT both!
+                (maybeBGotIt != nil && maybeCGotIt != nil).shouldBeFalse()
+                let theMessage = maybeBGotIt ?? maybeCGotIt ?? "<none>"
+                theMessage.shouldStartWith(prefix: "work:after-A-dead-\(i) at worker")
+            }
+        }
+        try pA.expectNoMessage(for: .milliseconds(50))
+
+        // we continue with stopping all remaining workers, after which the pool should terminate itself
+
+        workerB.tell("stop")
+        try pB.expectTerminated(workerB)
+        workerC.tell("stop")
+        try pC.expectTerminated(workerC)
+
+        try pW.expectTerminated(workers._ref)
+
+    }
+    func test_workerPool_static_throwOnEmptyInitialSet() throws {
+        let error = shouldThrow() {
+            let _: WorkerPoolRef <Never> = try WorkerPool.spawn(system, select: .static([]), name: "wrongConfigPool")
+        }
+
+        "\(error)".shouldContain("Illegal empty collection passed to `.static` worker pool")
     }
 }
