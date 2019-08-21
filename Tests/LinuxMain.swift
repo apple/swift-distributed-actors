280a520eb276ff302a807ea97e3f86a37c1098d9
tomer doron <tomer@apple.com>
2019-08-20 17:37:38 -0700

    regenerate linux tests (#2)
    motivation: prepare for CI
    
    changes: regenerate al linux tests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the Swift Distributed Actors open source project
 //
-// Copyright (c) 2018-2019 Apple Inc. and the Swift Distributed Actors project authors
+// Copyright (c) 2019 Apple Inc. and the Swift Distributed Actors project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
@@ -22,7 +22,7 @@ import XCTest
 
 #if os(Linux) || os(FreeBSD)
    @testable import CDistributedActorsMailboxTests
-   @testable import ConcurrencyHelpersTests
+   @testable import DistributedActorsConcurrencyHelpersTests
    @testable import DistributedActorsTestKitTests
    @testable import DistributedActorsTests
 
