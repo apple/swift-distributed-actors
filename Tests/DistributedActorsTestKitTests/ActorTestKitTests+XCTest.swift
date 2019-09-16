f24e0690f12bb48945e56ba90551043722c7796e
Dario Rexin <drexin@apple.com>
2019-09-16 11:29:42 -0700

    Generate Linux tests

--- Tests/DistributedActorsTestKitTests/ActorTestKitTests+XCTest.swift
+++ Tests/DistributedActorsTestKitTests/ActorTestKitTests+XCTest.swift
@@ -25,6 +25,7 @@ extension ActorTestKitTests {
         return [
             ("test_error_withoutMessage", test_error_withoutMessage),
             ("test_error_withMessage", test_error_withMessage),
+            ("test_fail_shouldNotImmediatelyFailWithinEventuallyBlock", test_fail_shouldNotImmediatelyFailWithinEventuallyBlock),
         ]
     }
 }
