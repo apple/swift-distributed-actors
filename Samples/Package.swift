e3a6887a8b91e16f657700f08ded2e9e81594802
Konrad `ktoso` Malawski <ktoso@apple.com>
2019-11-02 15:38:33 +0900

    Separate build for samples, so main build does not depend on random libs
    **Motivation:**
    
    To avoid the main project pulling in SwiftPrometheus etc, as we do not
    really use it anywhere in there, but only in the samples, so it should
    not show up in normal builds.
    
    **Modifications:**
    
    Separate Package.swift for all Samples
    
    **Result:**
    
    - Cleaner separation between samples dependencies and the project itself

---
+++ Samples/Package.swift
@@ -0,0 +1,81 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let targets: [PackageDescription.Target] = [
+    // ==== ----------------------------------------------------------------------------------------------------------------
+    // MARK: Samples
+
+    .target(
+        name: "SampleDiningPhilosophers",
+        dependencies: ["DistributedActors"],
+        path: "SampleDiningPhilosophers"
+    ),
+    .target(
+        name: "SampleLetItCrash",
+        dependencies: ["DistributedActors"],
+        path: "SampleLetItCrash"
+    ),
+    .target(
+        name: "SampleCluster",
+        dependencies: ["DistributedActors"],
+        path: "SampleCluster"
+    ),
+    .target(
+        name: "SampleMetrics",
+        dependencies: [
+            "DistributedActors",
+            "SwiftPrometheus",
+        ],
+        path: "SampleMetrics"
+    ),
+    .target(
+        name: "SampleGenActors",
+        dependencies: [
+            "DistributedActors"
+        ],
+        path: "SampleGenActors"
+    ),
+]
+
+var dependencies: [Package.Dependency] = [
+    // ~~~ parent ~~~
+    .package(path: "../"),
+    // ~~~ only for samples ~~~
+    .package(url: "https://github.com/MrLotU/SwiftPrometheus", .branch("master")),
+]
+
+let package = Package(
+    name: "swift-distributed-actors-samples",
+    products: [
+        /* ---  samples --- */
+
+        .executable(
+            name: "SampleDiningPhilosophers",
+            targets: ["SampleDiningPhilosophers"]
+        ),
+        .executable(
+            name: "SampleLetItCrash",
+            targets: ["SampleLetItCrash"]
+        ),
+        .executable(
+            name: "SampleCluster",
+            targets: ["SampleCluster"]
+        ),
+        .executable(
+            name: "SampleMetrics",
+            targets: ["SampleMetrics"]
+        ),
+        .executable(
+            name: "SampleGenActors",
+            targets: ["SampleGenActors"]
+        ),
+    ],
+
+    dependencies: dependencies,
+
+    targets: targets,
+
+    cxxLanguageStandard: .cxx11
+)
