03cd56848db0f0efba8a2bcf74708204a40415eb
Konrad `ktoso` Malawski <ktoso@apple.com>
2019-12-05 18:52:13 +0900

    Simplify Owned membership implementation thanks to new apply()

--- Tests/DistributedActorsTests/EventStreamTests.swift
+++ Tests/DistributedActorsTests/EventStreamTests.swift
@@ -90,8 +90,6 @@ final class EventStreamTests: XCTestCase {
 
         // we are sending a `.terminated` system message instead of stopping the probe here
         // because we still need to verify that we don't receive any more messages after that
-        //
-        // TODO: is there a less hacky way to do this?
         eventStream.ref.sendSystemMessage(.terminated(ref: p1.ref.asAddressable(), existenceConfirmed: true, addressTerminated: false))
         eventStream.ref.sendSystemMessage(.terminated(ref: p2.ref.asAddressable(), existenceConfirmed: true, addressTerminated: false))
 
