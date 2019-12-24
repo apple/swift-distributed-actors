32bf2d2c14128ab683d7425b87c7195a1543a7fd
Konrad `ktoso` Malawski <ktoso@apple.com>
2019-12-24 09:38:42 +0900

    =test,receptionist #335 Too aggressive test, removal may take a moment

--- Package.swift
+++ Package.swift
@@ -208,7 +208,7 @@ targets.append(contentsOf: [
 #endif
 
 var dependencies: [Package.Dependency] = [
-    .package(url: "https://github.com/apple/swift-nio.git", from: "2.8.0"),
+    .package(url: "https://github.com/apple/swift-nio.git", from: "2.12.0"),
     .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.2.0"),
     .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.2.0"),
 
