a1b85e8728202b64598705f17b89d0f601ae3b65
Konrad `ktoso` Malawski <ktoso@apple.com>
2020-03-04 22:06:50 +0900

    =cluster #497 update membershipSnapshot in all situations

--- Sources/DistributedActors/Cluster/ClusterControl.swift
+++ Sources/DistributedActors/Cluster/ClusterControl.swift
@@ -37,7 +37,7 @@ public struct ClusterControl {
         return self._membershipSnapshotHolder.membership
     }
 
-    internal func updateMembershipSnapshot(_ snapshot: Cluster.Membership) {
+    internal func updateMembershipSnapshot(_ snapshot: Cluster.Membership, file: String = #file, line: UInt = #line) {
         self.membershipSnapshotLock.lock()
         defer { self.membershipSnapshotLock.unlock() }
         self._membershipSnapshotHolder.membership = snapshot
@@ -76,6 +76,17 @@ public struct ClusterControl {
 
     public func join(node: Node) {
         self.ref.tell(.command(.initJoin(node)))
+    }
+
+    /// Usually NOT to be used, as having an instance of an `UniqueNode` in hand
+    /// is normally only possible after a handshake with the remote node has completed.
+    ///
+    /// However, in local testing scenarios, where the two nodes are executing in the same process (e.g. in a test),
+    /// this call saves the unwrapping of `cluster.node` into the generic node when joining them.
+    ///
+    /// - Parameter node: The node to be joined by this system.
+    public func join(node: UniqueNode) {
+        self.join(node: node.node)
     }
 
     public func leave() {
