e6c2edc1911b4faf89ea983350ae2200c575496f
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2020-07-11 13:11:42 +0900

    =more #availability fixes for iOS

--- Sources/DistributedActors/Serialization/Serialization+Manifest.swift
+++ Sources/DistributedActors/Serialization/Serialization+Manifest.swift
@@ -133,7 +133,7 @@ extension Serialization {
     @inline(__always)
     internal func getTypeHint(_ messageType: Any.Type) -> String {
         #if compiler(>=5.3)
-        if #available(macOS 10.16, *) {
+        if #available(macOS 10.16, iOS 14.0, *) {
             // This is "special". A manifest containing a mangled type name can be summoned if the type remains unchanged
             // on a receiving node. Summoning a type is basically `_typeByName` with extra checks that this type should be allowed
             // to be deserialized (thus, we can disallow decoding random messages for security).
