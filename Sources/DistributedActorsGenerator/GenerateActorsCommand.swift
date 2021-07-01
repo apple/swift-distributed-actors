eb04f3ada0073074ed129563903568e853dc995b
tomer doron <tomer@apple.com>
2021-06-30 23:03:49 -0700

    [DRAFT] distributed actors generator plugin (#801)
    * distributed actors generator plugin
    
    motivation: with SwiftPM gaining support for plugins, distributed actors generation can be become more seamless as such plugin
    
    changes:
    * rename GenActors code generation tool to DistributedActorsGenerator
    * refactor DistributedActorsGenerator to take source-directory, target-directory and number of buckets (for output "bucket" hashing), and change the logic to bucket-hash the generated code based on the first letter of the type.
    * create DistributedActorsGeneratorPlugin - a SwiftPM buildtool plugin to trigger DistributedActorsGenerator
    * apply the new plugin to the samples and relevant unit tests and remove checked-in GenActors directories that are no longer required
    
    * Apply suggestions from code review
    
    Co-authored-by: Yim Lee <yim_lee@apple.com>
    
    * fixup
    
    Co-authored-by: Konrad `ktoso` Malawski <ktoso@apple.com>
    Co-authored-by: Yim Lee <yim_lee@apple.com>

---
+++ Sources/DistributedActorsGenerator/GenerateActorsCommand.swift
@@ -0,0 +1,79 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Distributed Actors open source project
+//
+// Copyright (c) 2020 Apple Inc. and the Swift Distributed Actors project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.md for the list of Swift Distributed Actors project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import ArgumentParser
+import DistributedActors
+import Files
+import Foundation
+import Logging
+
+@main
+public struct GenerateActorsCommand: ParsableCommand {
+    @Option(help: "Print verbose information while analyzing and generating sources")
+    var logLevel: String = "info"
+
+    var logLevelValue: Logger.Level {
+        switch self.logLevel {
+        case "trace": return .trace
+        case "debug": return .debug
+        case "info": return .info
+        case "notice": return .notice
+        case "warning": return .warning
+        case "error": return .error
+        case "critical": return .critical
+        default: return .info
+        }
+    }
+
+    @Flag(name: .shortAndLong, help: "Print verbose all generated sources")
+    var printGenerated: Bool = false
+
+    @Option(help: "Source diretory to scan")
+    var sourceDirectory: String = ""
+
+    @Option(help: "Target diretory to generate in")
+    var targetDirectory: String = ""
+
+    @Option(help: "Number of buckets to generate")
+    var buckets: Int = 1
+
+    @Argument()
+    var targets: [String] = []
+
+    public init() {}
+}
+
+extension GenerateActorsCommand {
+    public func run() throws {
+        guard !self.sourceDirectory.isEmpty else {
+            fatalError("source directory is required")
+        }
+        guard !self.targetDirectory.isEmpty else {
+            fatalError("target directory is required")
+        }
+
+        let sourceDirectory = try Folder(path: self.sourceDirectory)
+
+        try FileManager.default.createDirectory(atPath: self.targetDirectory, withIntermediateDirectories: true)
+        let targetDirectory = try Folder(path: self.targetDirectory)
+
+        let generator = GenerateActors(logLevel: self.logLevelValue, printGenerated: self.printGenerated)
+        _ = try generator.run(
+            sourceDirectory: sourceDirectory,
+            targetDirectory: targetDirectory,
+            buckets: self.buckets,
+            targets: self.targets // ,
+        )
+    }
+}
