aa2f188966c8c4fd1e5ac412edac8637fd9c1492
Pop Flamingo <8317047+PopFlamingo@users.noreply.github.com>
2021-11-30 03:49:17 +0100

    Revert back to previous behavior where validate_format.sh actually formats the code

--- scripts/validate_format.sh
+++ scripts/validate_format.sh
@@ -22,10 +22,13 @@ printf "=> Checking format... "
 # check wether the config is ok
 swiftformat --dryrun . >/dev/null || { echo >&2 "'swiftformat' invocation failed."; exit 1; }
 
-# check wether the code is formatted correctly
-swiftformat . --lint
-if [ $? -ne 0 ]; then
+# format the code and exit with error if it was not formatted correctly
+FIRST_OUT="$(git status --porcelain)"
+swiftformat . > /dev/null 2>&1
+SECOND_OUT="$(git status --porcelain)"
+if [[ "$FIRST_OUT" != "$SECOND_OUT" ]]; then
   printf "\033[0;31mformatting issues!\033[0m\n"
+  git --no-pager diff
   exit 1
 else
   printf "\033[0;32mokay.\033[0m\n"
