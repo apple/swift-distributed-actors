6aa20d6ecfeee25f754f537eba54521d8de91b17
Kyle <kyle201817146@gmail.com>
2021-12-11 11:14:06 +0800

    Remove docc and docc render build

--- scripts/docs/generate_docc.sh
+++ scripts/docs/generate_docc.sh
@@ -35,47 +35,12 @@ modules=(
   DistributedActors
 )
 
-declare -r SWIFT="$TOOLCHAIN/usr/bin/swift"
-
-# all our public modules which we want to document, begin with `DistributedActors`
-modules=(
-  DistributedActors
-)
-
-declare -r build_path=".build/"
-declare -r build_path_linux="$build_path/"
-declare -r docc_source_path="$root_path/.build/swift-docc"
-declare -r docc_render_source_path="$root_path/.build/swift-docc-render"
-
-# Prepare and build docc
-if [[ ! -d "$docc_source_path" ]]; then
-  git clone https://github.com/apple/swift-docc.git "$docc_source_path"
-  cd $docc_source_path
-
-  if [[ ! -d "$docc_source_path/$build_path" ]]; then
-    swift build -c release
-  fi
-else
-  echo "Assuming docc is built..."
-fi
-
-
-if [[ ! -d "$docc_render_source_path" ]]; then
-  git clone https://github.com/apple/swift-docc-render.git "$docc_render_source_path"
-  cd $docc_render_source_path
-
-  npm install
-  npm run build
-else
-  echo "Assuming docc-render is built..."
-fi
-
-export DOCC_HTML_DIR=$docc_render_source_path/dist
-
 # Build documentation
 
 cd $root_path
 mkdir -p $root_path/.build/symbol-graphs
+
+declare -r SWIFT="$TOOLCHAIN/usr/bin/swift"
 
 for module in "${modules[@]}"; do
   echo "Building symbol-graph for module [$module]..."
