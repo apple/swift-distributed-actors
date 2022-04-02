329b9ba874dbf0f63e34d0e05141d14bd206a666
Kyle <kyle201817146@gmail.com>
2022-04-03 01:11:22 +0800

    Update Plugin implementation

--- Plugins/DistributedActorsGeneratorPlugin/Plugin.swift
+++ Plugins/DistributedActorsGeneratorPlugin/Plugin.swift
@@ -15,28 +15,28 @@
 import PackagePlugin
 
 @main struct MyPlugin: BuildToolPlugin {
-    func createBuildCommands(context: TargetBuildContext) throws -> [Command] {
+    func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        guard let target = target as? SourceModuleTarget else { return [] }
         let generatorPath = try context.tool(named: "DistributedActorsGenerator").path
-
-        let inputFiles = context.inputFiles.map { $0.path }.filter { $0.extension?.lowercased() == "swift" }
+        let inputFiles = target.sourceFiles.map { $0.path }.filter { $0.extension?.lowercased() == "swift" }
 
         let buckets = 5 // # of buckets for consistent hashing
         let outputFiles = !inputFiles.isEmpty ? (0 ..< buckets).map {
             context.pluginWorkDirectory.appending("GeneratedDistributedActors_\($0).swift")
         } : []
 
-        let command = Command.buildCommand(
-            displayName: "Generating distributed actors for target '\(context.targetName)'",
-            executable: generatorPath,
-            arguments: [
-                "--source-directory", context.targetDirectory.string,
-                "--target-directory", context.pluginWorkDirectory.string,
-                "--buckets", "\(buckets)",
-            ],
-            inputFiles: inputFiles,
-            outputFiles: outputFiles
-        )
-
-        return [command]
+        return [
+            .buildCommand(
+                displayName: "Generating distributed actors for target '\(target.name)'",
+                executable: generatorPath,
+                arguments: [
+                    "--source-directory", target.directory.string,
+                    "--target-directory", context.pluginWorkDirectory.string,
+                    "--buckets", "\(buckets)",
+                ],
+                inputFiles: inputFiles,
+                outputFiles: outputFiles
+            )
+        ]
     }
 }
