6733ddd961df0707dd03ea909ac218dc92970a93
Yim Lee <yim_lee@apple.com>
2022-05-18 17:30:14 -0700

    Fix compilation errors

--- Sources/DistributedActors/Cluster/Reception/OperationLogClusterReceptionist+Codable.swift
+++ Sources/DistributedActors/Cluster/Reception/OperationLogClusterReceptionist+Codable.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the Swift Distributed Actors open source project
 //
-// Copyright (c) 2020 Apple Inc. and the Swift Distributed Actors project authors
+// Copyright (c) 2020-2022 Apple Inc. and the Swift Distributed Actors project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
@@ -12,7 +12,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-public extension _OperationLogClusterReceptionist.ReceptionistOp {
+extension _OperationLogClusterReceptionist.ReceptionistOp {
     enum DiscriminatorKeys: String, Codable {
         case register
         case remove
