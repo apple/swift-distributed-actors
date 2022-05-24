33b5b0b94fd8a268c60234c48f2742dad9d10942
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2022-05-24 17:33:57 +0900

    [int] Temporarily disable integration tests

--- scripts/integration_tests.sh
+++ scripts/integration_tests.sh
@@ -13,5 +13,5 @@
 ##
 ##===----------------------------------------------------------------------===##
 
-mkdir -p .build # for the junit.xml file
-./IntegrationTests/run-tests.sh --junit-xml .build/junit-sh-tests.xml -i
+#mkdir -p .build # for the junit.xml file
+#./IntegrationTests/run-tests.sh --junit-xml .build/junit-sh-tests.xml -i
