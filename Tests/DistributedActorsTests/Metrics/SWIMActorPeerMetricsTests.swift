109caeb77d2371667eb323804c8c842c4210c608
Yim Lee <yim_lee@apple.com>
2022-06-22 18:18:27 -0700

    Update SWIM metrics tests

--- Tests/DistributedActorsTests/Metrics/SWIMActorPeerMetricsTests.swift
+++ Tests/DistributedActorsTests/Metrics/SWIMActorPeerMetricsTests.swift
@@ -125,51 +125,5 @@ final class ActorMetricsSWIMActorPeerMetricsTests: ClusteredActorSystemsXCTestCa
             }
         )!)
         counter.totalValue.shouldEqual(1)
-
-        /*
-         let first = await setUpNode("first")
-
-         let origin = testKit(first).makeTestProbe(expecting: SWIM.Message.self)
-         let target = testKit(first).makeTestProbe(expecting: SWIM.Message.self)
-         let through = testKit(first).makeTestProbe(expecting: SWIM.Message.self)
-
-         let fakeClusterRef = testKit(first).makeTestProbe(expecting: ClusterShell.Message.self).ref
-         let directive = SWIM.Instance.SendPingRequestDirective(
-             target: target.ref,
-             timeout: .seconds(1),
-             requestDetails: [
-                 .init(peerToPingRequestThrough: through.ref, payload: .none, sequenceNumber: 1),
-             ]
-         )
-
-         let instance = SWIM.Instance(settings: first.settings.swim, myself: origin.ref)
-         _ = try first._spawn("swim", of: SWIM.Message.self, .setup { context in
-             let shell = SWIMActorShell(instance, clusterRef: fakeClusterRef)
-             shell.sendPingRequests(directive, context: context) // we need a real context here since we reach into system metrics through it
-             return .receiveMessage { _ in .same }
-         })
-
-         switch try through.expectMessage() {
-         case .remote(.pingRequest(_, let pingOrigin, _, let sequenceNumber)):
-             // pretend we did a successful ping to the target
-             pingOrigin.ack(acknowledging: sequenceNumber, target: target.ref, incarnation: 10, payload: .none)
-         case let other:
-             fatalError("unexpected message: \(other)")
-         }
-
-         sleep(2) // FIXME: if we rework how throws work with eventually() we can avoid the sleep
-
-         let timerFirst = try self.metrics.expectTimer(instance.metrics.shell.pingRequestResponseTimeFirst)
-         pinfo("Recorded \(timerFirst): \(String(reflecting: timerFirst.lastValue.map { Duration.nanoseconds($0).prettyDescription }))")
-         timerFirst.label.shouldEqual("first.cluster.swim.roundTripTime.pingRequest")
-         timerFirst.lastValue!.shouldBeGreaterThan(0)
-
-         let timerAll = try self.metrics.expectTimer(instance.metrics.shell.pingRequestResponseTimeAll)
-         pinfo("Recorded \(timerAll): \(String(reflecting: timerAll.lastValue.map { Duration.nanoseconds($0).prettyDescription }))")
-         timerAll.label.shouldEqual("first.cluster.swim.roundTripTime.pingRequest")
-         timerAll.lastValue!.shouldBeGreaterThan(0)
-
-         try self.metrics.expectCounter(instance.metrics.shell.messageOutboundCount).totalValue.shouldEqual(1)
-         */
     }
 }
